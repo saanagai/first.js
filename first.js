0f0a33557973dfac83a5373d897ceaf80287d8e3
24LP0542 24LP0542 <24LP0542@admins-iMac.local>
2024-11-19 12:43:55 +0800

    angid

---
+++ first.js
@@ -0,0 +1,73 @@
+// let birthyear = 1985;
+// let nowyear = 2024;
+
+// let hariu = nowyear - birthyear;
+
+// console.log(hariu);
+
+// let title1 = "Baby shark";
+// let line1 = "Baby shark, doo, doo, doo, doo, doo, doo";
+
+// console.log(line1);
+// console.log(line1);
+// console.log(line1);
+// console.log(title1);
+
+// let line2 = "Mommy shark, doo, doo, doo, doo, doo, doo";
+// let title2 = "Mommy shark";
+// console.log(line2);
+// console.log(line2);
+// console.log(line2);
+// console.log(title2);
+
+// let title3 = "Daddy shark";
+// let line3 = "Daddy shark, doo, doo, doo, doo, doo, doo";
+// console.log(line3);
+// console.log(line3);
+// console.log(line3);
+// console.log(title3);
+// let title4 = "Grandma shark";
+// let line4 = "Grandma shark, doo, doo, doo, doo, doo, doo";
+// console.log(line4);
+// console.log(line4);
+// console.log(line4);
+// console.log(title4);
+// let title5 = "Grandpa shark";
+// let line5 = "Grandpa shark, doo, doo, doo, doo, doo, doo";
+// console.log(line5);
+// console.log(line5);
+// console.log(line5);
+// console.log(title5);
+// let title6 = "Run away";
+// let line6 = "Run away, doo, doo, doo, doo, doo, doo";
+// console.log(line6);
+// console.log(line6);
+// console.log(line6);
+// console.log(title6);
+// let title7 = "Safe at last";
+// let line7 = "Safe at last, doo, doo, doo, doo, doo, doo";
+// console.log(line7);
+// console.log(line7);
+// console.log(line7);
+// console.log(title7);
+// let title8 = "It's the end";
+// let line8 = "It's the end, doo, doo, doo, doo, doo, doo";
+// console.log(line8);
+// console.log(line8);
+// console.log(line8);
+// console.log(title8);
+let drom = false;
+let score = 17;
+if (score > 18) {
+  if (drom == true) {
+    console.log("passed");
+  } else {
+    console.log("dromdoo tentseh heregtei");
+  }
+} else {
+  if (drom == true) {
+    console.log("Duremdee tentseh heregtei");
+  } else {
+    console.log("durem bolon dromdo tentseh hergetei");
+  }
+}
---
+++ second.js
@@ -0,0 +1,37 @@
+// module operater
+
+let too = 33; // 7 bolon 11t huvaagddah//
+
+if (too % 7 == 0) {
+  if (too % 11 == 0) console.log("2land ni huvaagdana");
+  else {
+    console.log("11t huvaagdahgui,7d huvaagdana");
+  }
+} else {
+  if (too % 11 == 0) {
+    console.log("11t huvaagdana,7d huvaagdahgui");
+  } else {
+    console.log("2land ni huvaagdahgui");
+  }
+}
+
+// 1. Write a program that calculates the total cost of 3 apples and 2 bananas.
+// Each apple costs $1.5, and each banana costs $0.8.
+// Calculate the total cost and print it.
+
+let apple = 1.5 * 3;
+let banana = 0.8 * 2;
+let total = apple + banana;
+console.log(total);
+
+// 2. Write a program that checks if a number is even or odd.
+// Input: A number x.
+// Output: Print whether the number is even or odd.
+let too1 = 55;
+if (too1 % 2 == 0) {
+  if (too1 < 0) console.log("tegsh too surug");
+} else {
+  console.log("tegsh too eyreg");
+} else {
+    if(too1>1) console.log("sondgoi too ")
+}
